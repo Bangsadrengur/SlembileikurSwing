6c9bdf567621d79657712a65b3a87d442f7e9b51
Heimir Þór Kjartansson <hemmi.hk@gmail.com>
2012-03-05 15:05:10 +0000

    update

--- Vidmot.java
+++ Vidmot.java
@@ -101,6 +101,25 @@ public class Vidmot
                 statusLabel.setText("That is the correct number!");
             }
         }
+        private class Logik
+        {
+            // Fastayrðing gagna:
+            // number er slembitala til ágiskunar.
+            int number;
+
+            // Number er stillt, hún liggur á bilinu 1..10.
+            public Logik()
+            {
+                number = new java.util.Random().nextInt(9) + 1;
+            }
+
+            // Compares x to number. Negative value if lower, zero
+            // if equal and positive number if higher.
+            public int checkNumber(int x)
+            {
+                return new Integer(x).compareTo(this.number);
+            }
+        }
     }
 
     // Forrit gangsett.
